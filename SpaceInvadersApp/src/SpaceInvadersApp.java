e21b0cd841fdf10ff374143c552c72dbd49d4f53
Astramentis <WesleyFLVS@gmail.com>
2018-09-21 12:52:42 -0400

    Added enemy AI, bullets, base complete
    enemes now fire on Math function, player has death condition, enemies have death condition

--- SpaceInvadersApp/src/SpaceInvadersApp.java
+++ SpaceInvadersApp/src/SpaceInvadersApp.java
@@ -12,10 +12,12 @@ import java.util.stream.Collectors;
 
 public class SpaceInvadersApp extends Application {
     private Pane root = new Pane();
+    private double t = 0;
     private Sprite player = new Sprite(300, 750, 40,40, "player",Color.BLUE);
     private Parent createContent() {
         root.setPrefSize(600,800);
         root.getChildren().add(player);
+
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
@@ -23,10 +25,60 @@ public class SpaceInvadersApp extends Application {
             }
         };
         timer.start();
+        nextLevel();
         return root;
     }
-    private void shoot(){
+    private void nextLevel(){
+        for (int i = 0; i < 5; i++){
+            Sprite s = new Sprite(90 + i*100, 150, 30,30, "enemy", Color.RED);
+            root.getChildren().add(s);
+        }
+    }
+    private List<Sprite> sprites(){
+        return root.getChildren().stream().map(n->(Sprite)n).collect(Collectors.toList());
+    }
+    private void update(){
+        t += 0.016;
+        sprites().forEach(s->{
+            switch (s.type){
+                case "enemybullet":
+                    s.moveDown();
+                    if (s.getBoundsInParent().intersects(player.getBoundsInParent())){
+                        player.dead=true;
+                        s.dead=true;
+                    }
+                    break;
+                case "playerbullet":
+                    s.moveUp();
+                    sprites().stream().filter(e-> e.type.equals("enemy")).forEach(enemy -> {
+                        if (s.getBoundsInParent().intersects(enemy.getBoundsInParent())) {
+                            enemy.dead = true;
+                            s.dead = true;
+                        }
+                    });
+                    break;
+                case "enemy":
+                    if (t>2){
+                        if (Math.random()<0.3){
+                            shoot(s);
+                        }
+                    }
+                    break;
+            }
+        });
+        root.getChildren().removeIf(n-> {
+            Sprite s = (Sprite) n;
+            return s.dead;
+        });
 
+        if (t > 2){
+            t = 0;
+        }
+    }
+
+    private void shoot(Sprite who){
+        Sprite s = new Sprite((int)who.getTranslateX() + 20, (int)who.getTranslateY(), 5, 20, who.type + "bullet", Color.BLACK);
+        root.getChildren().add(s);
     }
 
     @Override
@@ -40,7 +92,10 @@ public class SpaceInvadersApp extends Application {
                             break;
                         case D:
                             player.moveRight();
+                            break;
                         case SPACE:
+                            shoot(player);
+                            break;
                     }
                 });
 
