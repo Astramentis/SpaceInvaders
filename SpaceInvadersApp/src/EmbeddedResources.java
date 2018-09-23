7c50c62786b4cbdaec150f15993b274856378fcd
Astramentis <WesleyFLVS@gmail.com>
2018-09-23 15:56:22 -0400

    Added image view, broke everything
    need to refactor into a proper JavaFX package to set up depedencies correctly, probably done with this for now

---
+++ SpaceInvadersApp/src/EmbeddedResources.java
@@ -0,0 +1,7 @@
+import sun.net.www.content.image.png;
+
+public class EmbeddedResources {
+    public EmbeddedResources(){
+        getClass().getResource("/res/10-512.png");
+    }
+}
--- SpaceInvadersApp/src/SpaceInvadersApp.java
+++ SpaceInvadersApp/src/SpaceInvadersApp.java
@@ -6,18 +6,26 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SpaceInvadersApp extends Application {
     private Pane root = new Pane();
     private double t = 0;
-    private Sprite player = new Sprite(300, 750, 40,40, "player",Color.BLUE);
+    private Sprite player = new Sprite(300, 750, 40, 40, "player", Color.BLUE);
+    ImageView image = new ImageView(new Image(getClass().getResourceAsStream("/res/10-512.png")));
+
+
     private Parent createContent() {
-        root.setPrefSize(600,800);
+        root.setPrefSize(600, 800);
         root.getChildren().add(player);
-        //TODO: remake in CSS styling
+        root.getChildren().add(image);
 
         AnimationTimer timer = new AnimationTimer() {
             @Override
@@ -29,29 +37,32 @@ public class SpaceInvadersApp extends Application {
         nextLevel();
         return root;
     }
-    private void nextLevel(){
-        for (int i = 0; i < 5; i++){
-            Sprite s = new Sprite(90 + i*100, 150, 30,30, "enemy", Color.RED);
+
+    private void nextLevel() {
+        for (int i = 0; i < 5; i++) {
+            Sprite s = new Sprite(90 + i * 100, 150, 30, 30, "enemy", Color.RED);
             root.getChildren().add(s);
         }
     }
-    private List<Sprite> sprites(){
-        return root.getChildren().stream().map(n->(Sprite)n).collect(Collectors.toList());
+
+    private List<Sprite> sprites() {
+        return root.getChildren().stream().map(n -> (Sprite) n).collect(Collectors.toList());
     }
-    private void update(){
+
+    private void update() {
         t += 0.016;
-        sprites().forEach(s->{
-            switch (s.type){
+        sprites().forEach(s -> {
+            switch (s.type) {
                 case "enemybullet":
                     s.moveDown();
-                    if (s.getBoundsInParent().intersects(player.getBoundsInParent())){
-                        player.dead=true;
-                        s.dead=true;
+                    if (s.getBoundsInParent().intersects(player.getBoundsInParent())) {
+                        player.dead = true;
+                        s.dead = true;
                     }
                     break;
                 case "playerbullet":
                     s.moveUp();
-                    sprites().stream().filter(e-> e.type.equals("enemy")).forEach(enemy -> {
+                    sprites().stream().filter(e -> e.type.equals("enemy")).forEach(enemy -> {
                         if (s.getBoundsInParent().intersects(enemy.getBoundsInParent())) {
                             enemy.dead = true;
                             s.dead = true;
@@ -59,26 +70,26 @@ public class SpaceInvadersApp extends Application {
                     });
                     break;
                 case "enemy":
-                    if (t>2){
-                        if (Math.random()<0.3){
+                    if (t > 2) {
+                        if (Math.random() < 0.3) {
                             shoot(s);
                         }
                     }
                     break;
             }
         });
-        root.getChildren().removeIf(n-> {
+        root.getChildren().removeIf(n -> {
             Sprite s = (Sprite) n;
             return s.dead;
         });
 
-        if (t > 2){
+        if (t > 2) {
             t = 0;
         }
     }
 
-    private void shoot(Sprite who){
-        Sprite s = new Sprite((int)who.getTranslateX() + 20, (int)who.getTranslateY(), 5, 20, who.type + "bullet", Color.BLACK);
+    private void shoot(Sprite who) {
+        Sprite s = new Sprite((int) who.getTranslateX() + 20, (int) who.getTranslateY(), 5, 20, who.type + "bullet", Color.BLACK);
         root.getChildren().add(s);
     }
 
@@ -86,21 +97,21 @@ public class SpaceInvadersApp extends Application {
     public void start(Stage stage) throws Exception {
 
         Scene scene = new Scene(createContent());
-        scene.setOnKeyPressed(e-> {
-                    switch (e.getCode()) {
-                        case A:
-                            player.moveLeft();
-                            break;
-                        case D:
-                            player.moveRight();
-                            break;
-                        case SPACE:
-                            shoot(player);
-                            break;
-                        case R:
-                            //TODO: add stage reset
-                    }
-                });
+        scene.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case A:
+                    player.moveLeft();
+                    break;
+                case D:
+                    player.moveRight();
+                    break;
+                case SPACE:
+                    shoot(player);
+                    break;
+                case R:
+                    //TODO: add stage reset
+            }
+        });
 
 
         stage.setScene(scene);
@@ -110,28 +121,34 @@ public class SpaceInvadersApp extends Application {
     private static class Sprite extends Rectangle {
         boolean dead = false;
         final String type;
-        Sprite(int x, int y, int w, int h, String type, Color color){
-            super(w,h,color);
+
+
+        Sprite(int x, int y, int w, int h, String type, Color color) {
+            super(w, h, color);
 
             this.type = type;
             setTranslateX(x);
             setTranslateY(y);
         }
-        void moveLeft(){
+
+        void moveLeft() {
             setTranslateX(getTranslateX() - 5);
         }
-        void moveRight(){
+
+        void moveRight() {
             setTranslateX(getTranslateX() + 5);
         }
-        void moveUp(){
+
+        void moveUp() {
             setTranslateY(getTranslateY() - 5);
         }
-        void moveDown(){
+
+        void moveDown() {
             setTranslateY(getTranslateY() + 5);
         }
     }
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
         launch(args);
     }
 
